c4f3fa97d3b995f82a81e0cd78f867ead45c43c7
okuyama <okuyama@engawa.jp>
2025-11-23 12:10:51 +0900

    javaのプロジェクトを作りました。

---
+++ src/test/java/com/example/filemap/MainTest.java
@@ -0,0 +1,12 @@
+package com.example.filemap;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+    @Test
+    public void testMain() {
+        assertTrue(true);
+    }
+}
+
